57de12c9d1d9e38f549acb057c9737129eeababf
Diogo Fernandes <diogosilvafernandes85@gmail.com>
2026-03-11 23:39:44 +0100

    feat: Refactor main application file to include container and router setup

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 """Main application file for the FastAPI web service."""
+from dotenv import load_dotenv
 import uvicorn
 from fastapi import FastAPI
 
+from app.config.container import Container
+from app.controllers import widgets_controller
+from app.services.widget_service.models import bucket_widget_provider
+
+container = Container()
+container.wire(modules=[widgets_controller, bucket_widget_provider])
+
 app = FastAPI()
+load_dotenv()
+
+app.include_router(widgets_controller.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
