035eccefe01bc0c444b3c529d174c078582fbea4
Diogo Fernandes <diogosilvafernandes85@gmail.com>
2026-03-25 22:57:44 +0100

    feat: update .env.example to include PORT variable and modify main.py to use PORT from environment

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 """Main application file for the FastAPI web service."""
+import os
 from dotenv import load_dotenv
 import uvicorn
 from fastapi import FastAPI
@@ -17,4 +18,4 @@ load_dotenv()
 app.include_router(widgets_controller.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", "8000")))
